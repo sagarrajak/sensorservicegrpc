a8807146ba07dfd1fced9b4e83d376cee397ebbf
sagar <sagar@192.168.1.16>
2025-09-15 16:43:38 +0530

    first commit

---
+++ src/main/java/com/sesor/grpc/sensorservicegrpc/SensorservicegrpcApplication.java
@@ -0,0 +1,13 @@
+package com.sesor.grpc.sensorservicegrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorservicegrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorservicegrpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesor/grpc/sensorservicegrpc/SensorservicegrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesor.grpc.sensorservicegrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorservicegrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
